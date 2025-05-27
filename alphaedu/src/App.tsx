ccddc86c5c8e4601eb6c1bd79b84880a2e8bd6c2
Star7-jpg <marcosconde612@gmail.com>
2025-05-27 13:45:05 -0600

    component About

--- alphaedu/src/App.tsx
+++ alphaedu/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import About from './components/About'
 
 function App() {
 
   return (
     <>
-      <h1>Hola</h1>      
+      <h1>Hola</h1>
+      <About />      
     </>
   )
 }
