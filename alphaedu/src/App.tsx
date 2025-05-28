4d902e953280e7d44d9d62b28c30b06b4ef15184
Abraham <abrahamperezquiroz0@gmail.com>
2025-05-27 20:59:02 -0600

    UwU

--- alphaedu/src/App.tsx
+++ alphaedu/src/App.tsx
@@ -3,9 +3,9 @@ import Footer from "./components/footer"
 
 export default function App() {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col ">
       <div className="flex-grow"></div>
-      <div className="border h-1 text font-extrabold">AlphaEdu Site <Footer/> </div>
+      <div className=" bg-blue-500 border h-1 text font-extrabold"> <Footer/> </div>
     </div>
 
   )
--- alphaedu/src/components/footer.tsx
+++ alphaedu/src/components/footer.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 export default function Footer() {
   return (
-    <div className=''>
-        <p>Gilipollas</p>
-    </div>
-  )
+    <footer className="bg-blue-500 text-center py-2 mt-4">
+      <p className="text-sm text-gray-600 ">AlphaEdu Site©Copyright.</p>
+    </footer>
+  );
 }
